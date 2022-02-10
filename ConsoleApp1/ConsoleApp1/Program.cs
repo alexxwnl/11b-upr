eda79941d4289ff9a2204a3c33507aed570e0d58
“alexxwnl” <alekscvetanov28@gmail.com>
2022-02-10 08:37:29 +0200

    nz

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string[] input = Console.ReadLine().Split().ToArray();
+int n =int.Parse(Console.ReadLine());  
+for (int i = 0; i < n; i++)
+{
+    string [] cmd = Console.ReadLine().Split().ToArray();   
+    string command = cmd[0];    
+    if (command=="Reverse")
+         {
+             input = input.Reverse().ToArray();
+         }
+         else if (command=="Distinct")
+         {
+             input=input.Distinct().ToArray();
+         }
+         else if (command=="Replace")
+         {
+             int index = int.Parse(cmd[1]);
+             string strForReplace = cmd[2]; 
+             input[index] = strForReplace;   
+         }
+     }
+        switch (command)
+        {
+           case "Reverse": input = input.Reverse().ToArray(); break;
+           case "Distinct": input = input.Distinct().ToArray(); break;
+           case "Replace": input [int.Parse(cmd[1])] = cmd[2]; break;
+}
+     Console.WriteLine(string.Join(", ",input));
