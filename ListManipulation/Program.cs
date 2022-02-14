4c54cff91bde17c54d1bebe3b4fbb0e57bbd14f5
alexxwnl <alekscvetanov28@gmail.com>
2022-02-14 10:32:51 +0200

    ListManipulation

--- ListManipulation/Program.cs
+++ ListManipulation/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace ListManipulation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                var command = input[0];
+                if (command == "print")
+                {
+                    Console.WriteLine(string.Join(" ", nums));
+                    break;
+                }
+                if (command == "add")
+                {
+                    int index = int.Parse(input[1]);
+                    int element = int.Parse(input[2]);
+                    nums.Insert(index, element); 
+                }
+                else if (command =="contains")
+                {
+                    int element = int.Parse(input[1]);
+                    Console.WriteLine(nums.IndexOf(element));
+                }
+                else if (command =="remove")
+                {
+                    int index = int.Parse(input[1]);
+                    nums.RemoveAt(index);
+                }
+            }
         }
     }
 }
