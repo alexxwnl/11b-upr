7265f72e9e79fedd764c6484d2f9e8834d4b304d
alexxwnl <alekscvetanov28@gmail.com>
2022-02-14 09:41:58 +0200

    add project

---
+++ ListManipulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
